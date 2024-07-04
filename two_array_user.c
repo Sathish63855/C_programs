e396f7bc29b90d43ca3fc5c3eaae4cb842329b5a
Sathish63855 <sathish995325@gmail.com>
2024-07-04 22:24:01 +0530

    sathish

--- two_array_user.c
+++ two_array_user.c
@@ -10,9 +10,30 @@ int main(){
 
 
     int **array = malloc(rows*sizeof(int *));
-    // for(int i=0;i<rows;i++){
-    //     array[i] = malloc(cols*sizeof(int));
-    // }
-    printf("%d\n",array);
-    printf("%d",array+3);
+    for(int i=0;i<rows;i++){
+        array[i] = malloc(cols*sizeof(int));
+    }
+
+    printf("Enter the elements of the array : \n");
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+            printf("Elements at [%d][%d] : ",i,j);
+            scanf("%d",&array[i][j]);
+        }
+    }
+    printf("The entered 2D array is : \n");
+    for(int i=0;i<rows;i++){
+        printf("\t");
+        for(int j=0;j<cols;j++){
+            printf("%d ",array[i][j]);
+        }
+        printf("\n");
+    }
+
+    for(int i=0;i<rows;i++){
+        free(array[i]);
+    }
+    free(array);
+    return 0;
+    
 }
